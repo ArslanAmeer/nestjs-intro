66fd912c30fa58613b57236f778d3f5914e411ba
ArslanAmeer <arslan.ameer@hotmail.com>
2023-01-31 13:48:48 +0500

    ♻️ Update: Products CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,18 +1,46 @@
-import { Controller, Post, Body } from "@nestjs/common";
-import { ProductsService } from "./products.service";
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductController {
+  constructor(private readonly productService: ProductsService) {}
 
-    constructor(private readonly productService: ProductsService) {}
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ): any {
+    const storedProductId = this.productService.insertProduct(
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    return { id: storedProductId };
+  }
 
-    @Post()
-    addProduct(
-        @Body('title') prodTitle: string,
-        @Body('description') prodDesc: string,
-        @Body('price') prodPrice: number
-        ): any {
-        const storedProductId = this.productService.insertProduct(prodTitle, prodDesc, prodPrice);
-        return {id: storedProductId}
-    }
+  @Get()
+  getAllProducts() {
+    return this.productService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productService.getSingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    return this.productService.updateProduct(
+      prodId,
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+  }
 }
